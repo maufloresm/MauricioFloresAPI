8bbeb73ec4c0f1269cb957899725fba34f33305d
Mauricio Flores <mauriciofm341@gmail.com>
2025-03-19 15:03:54 -0600

    Práctica 5

--- product/src/main/java/com/product/api/service/SvcCategory.java
+++ product/src/main/java/com/product/api/service/SvcCategory.java
@@ -1,8 +1,23 @@
 package com.product.api.service;
-import java.util.List;
+
+import com.product.api.commons.dto.ApiResponse;
+import com.product.api.dto.DtoCategoryIn;
 import com.product.api.entity.Category;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface SvcCategory {
-    public ResponseEntity<List<Category>> getCategories();
+
+    ResponseEntity<List<Category>> getCategories();
+    
+    ResponseEntity<List<Category>> getActiveCategories();
+    
+    ResponseEntity<ApiResponse> createCategory(DtoCategoryIn in);
+    
+    ResponseEntity<ApiResponse> updateCategory(DtoCategoryIn in, Integer id);
+    
+    ResponseEntity<ApiResponse> enableCategory(Integer id);
+    
+    ResponseEntity<ApiResponse> disableCategory(Integer id);
 }
